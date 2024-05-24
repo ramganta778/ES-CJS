c6db04a3e5c3b8964233900596ba58a8477162a0
ramganta778 <83300591+ramganta778@users.noreply.github.com>
2024-05-24 12:41:34 +0530

    Delete ES-CMS directory

--- ES-CMS/ES/server.js
+++
@@ -1,9 +0,0 @@
-import { founderOfJanasena,mainLeaders,aboutThisJanasena } from "./janasena.js";
-import { founderOfTDP,canditates,aboutThisTDP } from "./tdp.js";
-
-console.log(founderOfJanasena);
-console.log(mainLeaders);
-aboutThisJanasena();
-console.log(founderOfTDP);
-console.log(canditates);
-aboutThisTDP();
